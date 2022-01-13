fc80c915d7ac53fe1a4959dfec6329b52b5addeb
Your Name <comptonaustin82@gmail.com>
2022-01-12 22:28:21 -0800

    server handles favorited projects

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.post("/newuser", async (req, res) => {
         let {username, password, email} = {username : req.body.username, password : req.body.password, email : req.body.email }
         let insert = await db.query(`INSERT INTO USERS(username, email, password) values ('${username}', '${email}', '${password}')`)
         let result = await db.query(`SELECT * FROM USERS WHERE email = '${email}' AND password = '${password}' AND username = '${username}'`)
+        let projectTable = await db.query(`CREATE TABLE ${username}(ID SERIAL PRIMARY KEY, PROJECTNAME TEXT NOT NULL UNIQUE, PLATFORM TEXT NOT NULL, DISCORD TEXT UNIQUE, WEBSITE TEXT UNIQUE, TWITTER TEXT UNIQUE);`)
         console.log(result)
         res.status(200).json({
             status: "success",
@@ -58,6 +59,26 @@ app.post("/signin", async (req, res) => {
 })
 
 
+app.post("/bookmark", async (req, res)=> {
+    try{
+    console.log(req.body.username.trim(), req.body.projectname.trim(), req.body.platform.trim(), req.body.discord.trim(), req.body.website.trim(), req.body.twitter.trim())
+    let {username, projectname, platform, discord, website, twitter} = {
+        username : req.body.username.trim(),
+        projectname : req.body.projectname.trim(),
+        platform:req.body.platform.trim(),
+        discord : req.body.discord.trim(),
+        website : req.body.website.trim(),
+        twitter: req.body.twitter.trim()
+     }
+     let newproject = await db.query(`INSERT INTO ${username}(projectname, platform, discord, website, twitter) values ('${projectname}', '${platform}', '${discord}', '${website}', '${twitter}')`)
+     console.log(newproject)
+     res.status(200).json({
+         status : "success"
+     })
+    }catch(e){
+        console.log(e)
+    }
+})
 
 
 
