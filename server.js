874e116c2b77f0688d9fdb53ac157062a3948e54
Your Name <comptonaustin82@gmail.com>
2022-02-15 23:39:28 -0800

    cleanup

--- server.js
+++ server.js
@@ -32,11 +32,9 @@ app.post("/newuser", async (req, res) => {
       let {username, password, email} = {username : req.body.username, password : req.body.password, email : req.body.email }
       if(authenticateRegistration(username, password, email) == true){
         const hash = bcrypt.hashSync(password, bcrypt.genSaltSync());
-        console.log(hash)
         let insert = await db.query(`INSERT INTO USERS(username, email, password) values ('${username}', '${email}', '${hash}')`)
         let result = await db.query(`SELECT * FROM USERS WHERE email = '${email}' AND password = '${hash}' AND username = '${username}'`)
         let projectTable = await db.query(`CREATE TABLE ${username}(ID SERIAL PRIMARY KEY, PROJECTNAME TEXT NOT NULL UNIQUE, PLATFORM TEXT NOT NULL, DISCORD TEXT, WEBSITE TEXT, TWITTER TEXT);`)
-        console.log(result)
         res.status(200).json({
             status: "success",
             user: result.rows[0]
@@ -56,7 +54,6 @@ app.post("/signin", async (req, res) => {
         let {email, password} = {email: req.body.email, password : req.body.password}
         let result = await db.query(`SELECT * FROM USERS WHERE email = '${email}'`)
         const doesPasswordMatch = bcrypt.compareSync(password, result.rows[0].password)
-        console.log(doesPasswordMatch)
         if(doesPasswordMatch){
         res.status(200).json({
             status : "success",
@@ -74,7 +71,6 @@ app.post("/signin", async (req, res) => {
 
 app.post("/bookmark", async (req, res)=> {
     try{
-    console.log(req.body.discord, req.body.website, req.body.twitter)
     let {username, projectname, platform, discord, website, twitter} = {
         username : req.body.username.trim(),
         projectname : req.body.projectname.trim(),
@@ -84,7 +80,6 @@ app.post("/bookmark", async (req, res)=> {
         twitter: req.body.twitter == 'none' ? 'none' : req.body.twitter.trim()
      }
      let newproject = await db.query(`INSERT INTO ${username}(projectname, platform, discord, website, twitter) values ('${projectname}', '${platform}', '${discord}', '${website}', '${twitter}')`)
-     console.log(newproject)
      res.status(200).json({
          status : "success"
      })
@@ -100,7 +95,6 @@ app.post("/deletebookmark", async (req, res) => {
             projectname : req.body.projectname
         }
         let deletedProject = await db.query(`DELETE FROM ${username} WHERE projectname = '${projectname}'`)
-        console.log(deletedProject)
         res.status(200).json({
             status: "success"
         })
@@ -113,7 +107,6 @@ app.post("/getbookmarked", async (req, res) => {
     try {
         let userTable = req.body.username
         let favoritedProjects = await db.query(`SELECT * FROM ${userTable}`)
-        console.log(favoritedProjects.rows)
         res.status(200).json({
             status: "success",
             favoritedProjects: favoritedProjects.rows
